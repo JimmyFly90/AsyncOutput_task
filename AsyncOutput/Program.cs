2f2a57c8c672d6142539977d94a221aeaa220b0d
James Flynn <jamesdeclonflynn@hotmail.com>
2024-11-21 10:17:53 +0000

    Make methods async and add correct file path.

--- AsyncOutput/Program.cs
+++ AsyncOutput/Program.cs
@@ -1,24 +1,25 @@
 ﻿public class AsyncOutput
 {
     // Set the file path to the file you downloaded here
-    public static string filePath = "C:\\Users\\GAMER_PC\\Documents\\file.txt";
+    public static string filePath = "/Users/James.Flynn/Documents/AsyncOutput_task/AsyncOutput/file.txt";
 
-    public static void Main()
+    public static async Task Main()
     {
         Console.WriteLine("Started Program");
 
-        ReadFile(filePath);
+        Task fileReadingTask = ReadFile(filePath);
 
         Console.WriteLine("<< ASYNC CHECK >>");
+
+        await fileReadingTask;
 
         Console.WriteLine("Ended Program");
     }
 
-    public static string ReadFile(string fileName)
+    public static async Task ReadFile(string fileName)
     {
         Console.WriteLine("Started Reading File");
-        string text = File.ReadAllText(fileName);
+        string text = await File.ReadAllTextAsync(fileName);
         Console.WriteLine("Ended Reading File");
-        return text;
     }
 }
